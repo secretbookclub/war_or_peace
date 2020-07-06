660fb88d61b4338686d9f835e74df97262cb37a7
Nell Smart <moonmargin@gmail.com>
2020-07-05 21:46:53 -0600

    Save work on Turn class and test, try to make decks drop first card

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,23 +10,15 @@ class Turn
     @spoils_of_war = [] # basic and war
   end
 
-def type
-  if player_1.deck.rank_of_card_at(0) != player_2.deck.rank_of_card_at(0)
-    :basic
-  elsif player_1.deck.rank_of_card_at(2) == player_2.rank_of_card_at(2) && player_1.deck.rank_of_card_at(0) == player_2.deck.rank_of_card_at(0) # order doesn't matter
-    :mutually_assured_destruction
-  elsif player_1.deck.rank_of_card_at(0) == player_2.deck.rank_of_card_at(0) # if this were second condition, MAD would not happen
-    :war
+  def type
+    if player_1.deck.rank_of_card_at(0) != player_2.deck.rank_of_card_at(0)
+      :basic
+    elsif player_1.deck.rank_of_card_at(2) == player_2.rank_of_card_at(2) && player_1.deck.rank_of_card_at(0) == player_2.deck.rank_of_card_at(0) # order doesn't matter
+      :mutually_assured_destruction
+    elsif player_1.deck.rank_of_card_at(0) == player_2.deck.rank_of_card_at(0) # if this were second condition, MAD would not happen
+      :war
+    end
   end
-end
-
-  # what happens in a turn?
-  # Both players remove_card
-  # conditional statement to tell which rank is higher
-  # if one is higher, basic, 2 cards add to that player's array
-  # if equal,
-
-  # what previous method do you need to call?
 
   def winner
     if player_1.deck.rank_of_card_at(0) > player_2.deck.rank_of_card_at(0)
@@ -54,6 +46,11 @@ end
     end
   end
 
-  # def award_spoils
-  # end
+  def award_spoils(winner)
+    if player_1 == winner
+    player_1.deck.cards << @spoils_of_war
+    elsif player_2 == winner
+    player_2.deck.cards << @spoils_of_war
+    end
+  end
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -47,7 +47,7 @@ class TurnTest < Minitest::Test
     player_2 = Player.new(deck_2, 'Sandy')
 
     turn = Turn.new(player_1, player_2)
-# binding.pry
+
     assert_equal :basic, turn.type
   end
 
@@ -91,13 +91,33 @@ class TurnTest < Minitest::Test
     player_2 = Player.new(deck_2, 'Sandy')
 
     turn = Turn.new(player_1, player_2)
-    binding.pry
+
     assert_equal [card_1, card_3], turn.pile_cards
   end
 
   def test_can_award_spoils
     # skip
+    card_1 = Card.new(:heart, 'Jack', 11)
+    card_2 = Card.new(:heart, '10', 10)
+    card_3 = Card.new(:heart, '9', 9)
+    card_4 = Card.new(:diamond, 'Jack', 11)
+    card_5 = Card.new(:heart, '8', 8)
+    card_6 = Card.new(:diamond, 'Queen', 12)
+    card_7 = Card.new(:heart, '3', 3)
+    card_8 = Card.new(:diamond, '2', 2)
 
+    deck_1 = Deck.new([card_1, card_2, card_5, card_8])
+    deck_2 = Deck.new([card_3, card_4, card_6, card_7])
+
+    player_1 = Player.new(deck_1, 'Nero')
+    player_2 = Player.new(deck_2, 'Sandy')
+
+    turn = Turn.new(player_1, player_2)
+
+    winner = turn.winner
+    # turn.award_spoils(winner)
+# binding.pry
+    assert_equal [card_1, card_3], turn.award_spoils(winner)
   end
 
 end
